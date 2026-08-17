98e308ccf83b8aac2e3efcf629014ec29f5f8254
Arthur Galvão <arthurgmgalvao@gmail.com>
2026-08-17 13:44:02 -0300

    Primeiro commit, primeiro exercício de calcular meses e dias com a idade

---
+++ treino.py
@@ -0,0 +1,7 @@
+print("Quantos anos você tem?");
+idade = int(input());
+meses = idade * 12;
+dias = idade * 360;
+print(f"Você tem {idade} anos;");
+print(f"{meses} meses;");
+print(f"E {dias} dias.");
