1a2a1c59d0071d1d9337efb90d2a6466237924c2
Arthur Galvão <arthurgmgalvao@gmail.com>
2026-08-18 10:56:27 -0300

    Exercício 7 python find

---
+++ aula2/exercicio7.py
@@ -0,0 +1,5 @@
+print("Escreva uma frase e uma palavra-chave")
+palavra_chave = input("Palavra-Chave:")
+frase = input("Frase:")
+
+print(f"Qual é a posição da primeira palavra chave? {frase.find(palavra_chave)}")
