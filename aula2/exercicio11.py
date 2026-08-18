5da71defa68b6798d3396e1a4e59af416af09302
Arthur Galvão <arthurgmgalvao@gmail.com>
2026-08-18 16:23:37 -0300

    Exercício 11 python array for

---
+++ aula2/exercicio11.py
@@ -0,0 +1,7 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+contagem = 0
+for i in numeros:
+  contagem += 1
+print("Quantos elementos tem:",contagem)
+print("Maior número:",max(numeros))
+print("Menor número:",min(numeros))
