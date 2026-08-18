0171b2827da80c719397b38933a0798782d8010e
Arthur Galvão <arthurgmgalvao@gmail.com>
2026-08-18 10:43:16 -0300

    Exercício 6 python count

---
+++ aula2/exercicio6.py
@@ -0,0 +1,5 @@
+print("Escreva uma frase e uma palavra-chave")
+frase = input("Frase:")
+palavra_chave = input("Palavra-Chave:")
+
+print(f"A palavra chave aparece {frase.count(palavra_chave)} vezes na frase")
