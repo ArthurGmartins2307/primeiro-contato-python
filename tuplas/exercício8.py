09c543bb2c5dbfff067c5db09d7f720b5d7084e0
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-09-05 18:58:25 -0300

    exercício 8 tuplas

--- tuplas/exercício8.py
+++ tuplas/exercício8.py
@@ -1,7 +1,17 @@
-valor_conta = float(input('Qual foi o valor total da compra? '))
-taxa_de_servico = valor_conta * 0.10
-valor_total = valor_conta + taxa_de_servico
-quantidade_pessoas = int(input('Quantas pessoas vão dividir? '))
-juncao = (quantidade_pessoas, valor_total)
-print(f'Quantidade total de pessoa: {juncao[0]} - Valor para cada pessoa: {juncao[1] / juncao[0]}')
+jogadores = []
+
+quantidade = int(input('Quantos jogadores? '))
+
+for i in range(quantidade):
+    nome = input('Nome do jogador: ')
+    pontuacao = int(input('Pontuação: '))
+
+    jogadores.append((nome, pontuacao))
+
+jogadores.sort(key=lambda jogador: jogador[1], reverse=True)
+
+print('\nRanking:')
+
+for i in range(len(jogadores)):
+    print(f'{i + 1}º - {jogadores[i][0]}: {jogadores[i][1]} pontos')
 
