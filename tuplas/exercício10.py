873bf27d725146990ff5f670cd8cbf4e03596a20
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-09-05 19:00:53 -0300

    exercício 10 tuplas

--- tuplas/exercício10.py
+++ tuplas/exercício10.py
@@ -1,7 +1,7 @@
-valor_conta = float(input('Qual foi o valor total da compra? '))
-taxa_de_servico = valor_conta * 0.10
-valor_total = valor_conta + taxa_de_servico
-quantidade_pessoas = int(input('Quantas pessoas vão dividir? '))
-juncao = (quantidade_pessoas, valor_total)
-print(f'Quantidade total de pessoa: {juncao[0]} - Valor para cada pessoa: {juncao[1] / juncao[0]}')
-
+import math
+valor_aluguel = float(input('Qual é o valor do aluguel? '))
+despesas = float(input('Qual foi o valor das despesas? '))
+pessoas = int(input('Quantas pessoas foram? '))
+valor_total = valor_aluguel + despesas
+dividir = math.ceil(valor_total / pessoas)
+print(f'Valor que cada um deverá que pagar: {dividir}')
