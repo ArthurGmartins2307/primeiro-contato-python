3592e621403b93f414f55b9fff55c37c3e6facd1
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-09-06 18:32:41 -0300

    Exercício 4 Dicionários

--- dicionario/exercício4.py
+++ dicionario/exercício4.py
@@ -1,1 +1,17 @@
+chaves = ['nome', 'idade', 'nota']
+alunos = []
 
+def criar_aluno():
+  nome = input('Qual é o nome do aluno? ')
+  idade = int(input('Qual é a idade do aluno? '))
+  nota = float(input('Qual foi a nota do aluno? '))
+  aluno = [nome, idade, nota]
+  juntar2= zip(chaves, aluno)
+  dicionario2 = dict(juntar2)
+  alunos.append(dicionario2)
+
+def mostrar_aluno(int):
+  print(alunos[int])
+
+criar_aluno()
+mostrar_aluno(0)
