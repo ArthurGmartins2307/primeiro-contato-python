a001abac21812ab2934494cdb2f01e1309a4f3ca
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-09-06 18:40:57 -0300

    Exercício 7 Dicionários

--- dicionario/exercício7.py
+++ dicionario/exercício7.py
@@ -1,1 +1,13 @@
+frase = input('Escreva uma frase: ')
+palavras = frase.split()
+letras = []
 
+for item in palavras:
+  letras_palavra = 0
+  for i in item:
+    letras_palavra += 1
+  letras.append(letras_palavra)
+
+juntar = zip(palavras, letras)
+dicionario = dict(juntar)
+print(dicionario)
