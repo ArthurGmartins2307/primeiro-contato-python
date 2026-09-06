8632a7d099f790f071e82c795c0124aefba43d5e
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-09-06 18:41:17 -0300

    Exercício 8 Dicionários

--- dicionario/exercício8.py
+++ dicionario/exercício8.py
@@ -1,1 +1,23 @@
 
+telefones = {
+    "João": "11987654321",
+    "Maria": "11912345678",
+    "Pedro": "11955556666",
+    "Ana": "11999998888",
+    "Carlos": "11977776666",
+    "Juliana": "11944443333",
+    "Lucas": "11922221111",
+    "Fernanda": "11988887777"
+}
+
+def adicionar_contato(nome, numero):
+  telefones[nome] = numero
+
+def remover_contato(nome):
+  del telefones[nome]
+
+
+adicionar_contato('Neide', 11995726648)
+print(telefones)
+remover_contato('João')
+print(telefones)
