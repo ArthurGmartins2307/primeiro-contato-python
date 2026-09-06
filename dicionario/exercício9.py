f2228837f7d463bee48d0e26f60998e5098a3a37
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-09-06 19:01:51 -0300

    Exercício 9 Dicionários

--- dicionario/exercício9.py
+++ dicionario/exercício9.py
@@ -1,1 +1,20 @@
 
+alunos = {
+    "João": [8.5, 7.0, 9.0],
+    "Maria": [10.0, 8.5, 9.5],
+    "Pedro": [6.0, 7.5, 8.0],
+    "Ana": [9.0, 9.5, 10.0],
+    "Carlos": [7.0, 6.5, 8.5]
+}
+
+def adicionar_nota(nome, nota):
+  alunos[nome].append(nota)
+
+def visualizar_media(nome):
+  soma = 0
+  for i in alunos[nome]:
+    soma += i
+  media = soma / len(alunos[nome])
+  print(f'Média do(a) aluno(a): {media}')
+
+visualizar_media('Ana')
