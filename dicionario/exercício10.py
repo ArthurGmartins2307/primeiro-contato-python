11a21b37e1eaa423b2d146b0c1babfcca8035045
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-09-06 19:11:03 -0300

    Exercício 10 Dicionários

--- dicionario/exercício10.py
+++ dicionario/exercício10.py
@@ -1,1 +1,15 @@
 
+def contagem(palavras):
+  palavras.split()
+  contagem = {}
+
+  for item in palavras:
+    for i in item:
+      if i != ' ':
+        if i in contagem:
+          contagem[i] += 1
+        else:
+          contagem[i] = 1
+  print(contagem)
+
+contagem('palavras que se repetem')
