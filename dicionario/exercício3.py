5ad660ad240947012d4df3321962f7157eb00bb9
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-09-06 18:32:11 -0300

    Exercício 3 Dicionários

--- dicionario/exercício3.py
+++ dicionario/exercício3.py
@@ -1,1 +1,19 @@
+def contar_palavras(frase):
+    palavras = frase.split()
 
+    contagem = {}
+
+    for palavra in palavras:
+        if palavra in contagem:
+            contagem[palavra] += 1
+        else:
+            contagem[palavra] = 1
+
+    return contagem
+
+
+frase = input("Digite uma frase: ")
+
+resultado = contar_palavras(frase)
+
+print(resultado)
